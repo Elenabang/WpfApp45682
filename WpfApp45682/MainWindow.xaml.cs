3f258d032849bb2cc0ba915f81a199ae6411420f
ChervontsevaAnna <anna.chervontseva@bk.ru>
2021-11-25 09:04:02 +0300

    Добавьте файлы проекта.

---
+++ WpfApp45682/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp45682
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Game game;
+        room room;
+        public Start()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte n;
+            bool success = byte.TryParse(Gamers.Text, out n);
+            if (success)
+            {
+                if (n <= 1 | n > 10)
+                {
+                    Error.Content = "Введите число от 2 до 10";
+                }
+                else
+                {
+                    game = new Game(n);
+                    room = new Room();
+                    room.Show();
+                    window.WindowState = WindowState.Minimized;
+                }
+
+            }
+            else
+            {
+                Error.Content = "Введите число от 2 до 10";
+            }
+        }
+    }
+}
---
+++ WpfApp45682/room.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pap
+{
+    /// <summary>
+    /// Логика взаимодействия для room.xaml
+    /// </summary>
+    public partial class room : Window
+    {
+            {
+        enum Direction { left, right, up, down, };
+        double x = 0;
+        double y = 0;
+        public Room()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(MovePlayer);
+            timer.Start();
+        }
+        private void MovePlayer(object sender, EventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Left))
+            {
+                x -= .025;
+                Canvas.SetLeft(Player, x);
+            }
+            else if (Keyboard.IsKeyDown(Key.Up))
+            {
+                y -= .025;
+                Canvas.SetTop(Player, y);
+            }
+            else if (Keyboard.IsKeyDown(Key.Down))
+            {
+                y += .025;
+                Canvas.SetTop(Player, y);
+            }
+            else if (Keyboard.IsKeyDown(Key.Right))
+            {
+                x += .025;
+                Canvas.SetLeft(Player, x);
+            }
+    }
+}
